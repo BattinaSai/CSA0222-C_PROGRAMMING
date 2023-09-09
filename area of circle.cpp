081ca4c3b6349904b9f66775038135986c4b166f
BattinaSai <124449457+BattinaSai@users.noreply.github.com>
2023-09-09 13:16:33 +0530

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int r;
+	float pie=3.14,area;
+	printf("enter the radius:");
+	scanf("%d",&r);
+	area=(pie*r*r);
+	printf("area of circle is %f:",area);
+	return 0;
+}
